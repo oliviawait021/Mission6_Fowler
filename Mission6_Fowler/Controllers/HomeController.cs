b5f748b352f747282803cc817d772eb465e88e03
oliviafowler02 <130636149+oliviafowler02@users.noreply.github.com>
2025-02-18 13:16:14 -0700

    finishing things up

--- Mission6_Fowler/Controllers/HomeController.cs
+++ Mission6_Fowler/Controllers/HomeController.cs
@@ -4,53 +4,98 @@ using Microsoft.EntityFrameworkCore;
 using Mission6_Fowler.Models;
 using SQLitePCL;
 
-namespace Mission6_Fowler.Controllers;
-
-public class HomeController : Controller
+namespace Mission6_Fowler.Controllers
 {
-    private MovieContext _context;
+    // HomeController handles requests related to movie data management
+    public class HomeController : Controller
+    {
+        private MovieContext _context; // Declares the context to interact with the database
 
-    public HomeController(MovieContext someName) // Constructor
-    {
-        _context = someName;
-    }
-
-    public IActionResult Index() // Index View
-    {
-        return View();
-    }
-
-    public IActionResult KnowJoel() // Get to know Joel page
-    {
-        return View();
-    }
-    [HttpGet]
-    public IActionResult MovieForm() // Get for the movie form page
-    {
-        ViewBag.Categories =  _context.Categories.ToList();
-        return View(new Form());
-    }
-
-    [HttpPost]
-    public IActionResult MovieForm(Form response) // post of the Movie form page
-    {
-        if (ModelState.IsValid)
+        // Constructor that initializes the controller with a MovieContext
+        public HomeController(MovieContext someName) 
         {
-            _context.Movies.Add(response);
-            _context.SaveChanges(); 
-            return View("Confirmation", response);
+            _context = someName; // Initializes the _context with the injected database context
         }
-        else
+
+        // Action that renders the Index view
+        public IActionResult Index() 
         {
-            ViewBag.Categories =  _context.Categories.ToList();
-            return View(response);
+            return View(); // Returns the default view for the homepage
         }
-    }
-    
-    public IActionResult MovieList()
-    {
-        var movies = _context.Movies.Include(x => x.Category).ToList();
-        
-        return View(movies);
+
+        // Action that renders a "Know Joel" page
+        public IActionResult KnowJoel() 
+        {
+            return View(); // Returns the view for the "Know Joel" page
+        }
+
+        // GET action that renders the MovieForm for adding a new movie
+        [HttpGet]
+        public IActionResult MovieForm() 
+        {
+            ViewBag.Categories = _context.Categories.ToList(); // Passes categories to the view
+            return View(new Form()); // Initializes a new Form object to be used in the view
+        }
+
+        // POST action that processes the form submission for adding a new movie
+        [HttpPost]
+        public IActionResult MovieForm(Form response) 
+        {
+            // Check if the model is valid (based on the form data)
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(response); // Adds the new movie to the database
+                _context.SaveChanges(); // Saves changes to the database
+                return View("Confirmation", response); // Displays the confirmation view
+            }
+            else
+            {
+                // If the form is invalid, reload categories and return the view with the current form data
+                ViewBag.Categories = _context.Categories.ToList();
+                return View(response);
+            }
+        }
+
+        // Action that fetches and displays the collection of movies
+        public IActionResult MovieCollection()
+        {
+            var movies = _context.Movies.Include(x => x.Category).ToList(); // Fetches all movies with their associated category
+            return View(movies); // Returns the view with the list of movies
+        }
+
+        // GET action for editing an existing movie record based on its ID
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _context.Movies.Single(x => x.MovieId == id); // Fetches the movie record to be edited
+            ViewBag.Categories = _context.Categories.ToList(); // Passes categories to the view
+            return View("MovieForm", recordToEdit); // Returns the movie form view populated with the existing movie data
+        }
+
+        // POST action that processes the updated movie data and saves changes to the database
+        [HttpPost]
+        public IActionResult Edit(Form updatedMovie)
+        {
+            _context.Update(updatedMovie); // Updates the movie record in the database
+            _context.SaveChanges(); // Saves the changes to the database
+            return RedirectToAction("MovieCollection"); // Redirects to the movie collection page after saving
+        }
+
+        // GET action to render the delete confirmation page for a specific movie
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Movies.Single(x => x.MovieId == id); // Fetches the movie record to be deleted
+            return View("Delete", recordToDelete); // Returns the delete confirmation view with the movie data
+        }
+
+        // POST action to handle the deletion of a movie
+        [HttpPost]
+        public IActionResult Delete(Form recordToDelete)
+        {
+            _context.Movies.Remove(recordToDelete); // Removes the movie record from the database
+            _context.SaveChanges(); // Saves the changes (deletes the movie)
+            return RedirectToAction("MovieCollection"); // Redirects to the movie collection page after deletion
+        }
     }
 }
--- Mission6_Fowler/Models/Form.cs
+++ Mission6_Fowler/Models/Form.cs
@@ -9,18 +9,17 @@ public class Form // create database format
     [Required]
     public int MovieId { get; set; }
     
-    [ForeignKey("CatgoryId")]
+    [ForeignKey("CatgoryId")] // foreign key connecting different table
     public int CategoryId { get; set; }
-    public Categories Category { get; set; }
+    public Categories? Category { get; set; }
     [Required(ErrorMessage = "Please enter the movie title.")]
     public string Title { get; set; }
     [Required]
     [Range(1880, int.MaxValue, ErrorMessage = "Year must be 1880 or later.")]
     public int Year { get; set; }
-    public string Director { get; set; }
-    public string Rating { get; set; }
+    public string? Director { get; set; }
+    public string? Rating { get; set; }
     [Required(ErrorMessage = "Please enter if the movie has been edited.")]
-
     public bool Edited { get; set; }
     public string? LentTo { get; set; }
     [Required(ErrorMessage = "Please enter if the movie has been copied to plex.")]
